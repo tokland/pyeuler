481ce2732ecd7fa41a331e433d1c549cebe46169
Arnau Sanchez <pyarnau@gmail.com>
2010-09-27 15:35:15 +0200

    problem 42 (data file)

--- pyeuler/problems.py
+++ pyeuler/problems.py
@@ -380,3 +380,17 @@ def problem42():
     dictionary = dict((c, n) for (n, c) in enumerate(string.ascii_uppercase, 1))
     words = data.openfile("words.txt").read().replace('"', '').split(",")
     return ilen(word for word in words if is_triangle(sum(dictionary[c] for c in word)))
+
+def problem43():
+    """The number, 1406357289, is a 0 to 9 pandigital number because it is made 
+    up of each of the digits 0 to 9 in some order, but it also has a rather 
+    interesting sub-string divisibility property. Let d1 be the 1st digit, d2 
+    be the 2nd digit, and so on. In this way, we note the following: 
+    d2d3d4=406 is divisible by 2, d3d4d5=063 is divisible by 3, 
+    d4d5d6=635 is divisible by 5, d5d6d7=357 is divisible by 7,
+    d6d7d8=572 is divisible by 11, d7d8d9=728 is divisible by 13,
+    d8d9d10=289 is divisible by 17. Find the sum of all 0 to 9 pandigital 
+    numbers with this property."""
+    divisors = take(7, primes())
+    return list(divisors)
+    
